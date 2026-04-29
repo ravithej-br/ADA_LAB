ab2aa23765d4bdbe0aa7d2fa784349e60524f41f
Ravitheja B R <ravithejabr.cs25@bmsce.ac.in>
2026-04-29 09:40:33 +0530

    Add files via upload

---
+++ knapsack_greedy_method.c
@@ -0,0 +1,77 @@
+/*
+FractionalKnapsack(W, items)
+    Sort items in decreasing order of (profit/weight)
+    TotalProfit = 0
+    For each item i in items:
+        If weight[i] &lt;= W:
+            Take full item
+            W = W – weight[i]
+            TotalProfit += profit[i]
+        Else:
+            Fraction = W / weight[i]
+            TotalProfit += profit[i] * Fraction
+            Break
+    Return TotalProfit 
+*/
+
+#include <stdio.h>
+
+void main() {
+    int capacity, no_items, cur_weight, item;
+    int used[10];
+    float total_profit = 0.0;   // initialize to 0
+    int i;
+    int weight[10];
+    int value[10];
+
+    printf("Enter the capacity of knapsack:\n");
+    scanf("%d", &capacity);
+
+    printf("Enter the number of items:\n");
+    scanf("%d", &no_items);
+
+    printf("Enter the weight and value of %d items:\n", no_items);
+    for (i = 0; i < no_items; i++) {
+        printf("Weight[%d]: ", i);
+        scanf("%d", &weight[i]);
+        printf("Value[%d]: ", i);
+        scanf("%d", &value[i]);
+    }
+
+    for (i = 0; i < no_items; ++i)
+        used[i] = 0;
+
+    cur_weight = capacity;
+
+    while (cur_weight > 0) {
+        item = -1;
+
+        // Select item with max value/weight ratio among unused
+        for (i = 0; i < no_items; ++i) {
+            if (used[i] == 0 &&
+                (item == -1 || (float)value[i] / weight[i] > (float)value[item] / weight[item])) {
+                item = i;
+            }
+        }
+
+        if (item == -1) break; // no item left
+
+        used[item] = 1;
+        cur_weight -= weight[item];
+        total_profit += value[item];
+
+        if (cur_weight >= 0) {
+            printf("\nAdded object %d (%d Rs., %dKg) completely in the bag. Space left: %d.\n",
+                   item + 1, value[item], weight[item], cur_weight);
+        } else {
+            int item_percent = (int)((1 + (float)cur_weight / weight[item]) * 100);
+            printf("\nAdded %d%% (%d Rs., %dKg) of object %d in the bag.\n",
+                   item_percent, value[item], weight[item], item + 1);
+
+            total_profit -= value[item]; // remove full value
+            total_profit += (1 + (float)cur_weight / weight[item]) * value[item]; // add fraction
+        }
+    }
+
+    printf("\nFilled the bag with objects worth %.2f Rs.\n", total_profit);
+}
